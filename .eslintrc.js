954ea38440a862e484a270c23e20961ec668f56d
Doug Allrich <dougallrich@gmail.com>
2019-12-17 19:29:50 -0600

    chore: set up vuetify and tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,19 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': ['error', 'always'],
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never',
+    }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'function-paren-newline': ['error', 'consistent'],
+    'linebreak-style': ['error', 'windows'],
+    'object-shorthand': ['error', 'always', { avoidQuotes: false }],
+    'no-nested-ternary': 'off',
+    'no-return-assign': ['error', 'except-parens'],
+    'no-cond-assign': ['error', 'except-parens'],
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
   },
   parserOptions: {
     parser: 'babel-eslint',
